aba0e2d6011a8a12033b88702f1ca3dce0b620ae
sachin barali <sachinbarali41@gmail.com>
2024-04-22 20:43:30 +0545

    put request for user update

--- frontend/src/components/Message.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Alert } from "react-bootstrap";
-
-function Message({ varient, children }) {
-  return <Alert variant={varient}>{children}</Alert>;
-}
-
-export default Message;
