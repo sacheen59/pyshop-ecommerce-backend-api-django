aa59e4c1d5859b2d417da42240206315042758ec
sachin barali <sachinbarali41@gmail.com>
2024-04-22 20:36:39 +0545

    put request for user update

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('register/',user_views.register_user, name="register"),
     path('', user_views.get_users, name="users"),
     path('profile/',user_views.get_user_profile, name = 'user-profile'),
+    path('profile/update/',user_views.update_user_profile, name = "update-userProfile"),
 ]
